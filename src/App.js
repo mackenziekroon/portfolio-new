7e14a780008860e89407e2cdc5a710525b6491c0
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-02-17 16:31:12 -0800

    adding bio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="heading">
+      <h1>Hello World, I'm Mackenzie Kroon</h1>
+      <p>I am a Software Engineer.</p>
+      <p>
+        I recently graduated from the Grace Hopper program at Fullstack Academy.
+      </p>
     </div>
   );
 }
