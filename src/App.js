0a05cdb7dfd44ad0eeb67edc8f36a0ad04d2f913
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-02-17 16:39:57 -0800

    css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="heading">
-      <h1>Hello World, I'm Mackenzie Kroon</h1>
-      <p>I am a Software Engineer.</p>
-      <p>
-        I recently graduated from the Grace Hopper program at Fullstack Academy.
-      </p>
+    <div>
+      <div className="heading-container">
+        <div className="heading">
+          <h1>Hello World, I'm Mackenzie Kroon</h1>
+          <p>I am a Software Engineer.</p>
+          <p>
+            I recently graduated from the Grace Hopper program at Fullstack
+            Academy.
+          </p>
+        </div>
+      </div>
+      <div className="bio-container">
+        <div className="bio">
+          <h1>next section</h1>
+        </div>
+      </div>
     </div>
   );
 }
