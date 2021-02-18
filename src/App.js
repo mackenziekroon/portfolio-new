587d94fef1191f906ccf42665ccd7376ef3875c5
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-02-17 16:59:54 -0800

    css: fixed line border

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ function App() {
       <div className="heading-container">
         <div className="heading">
           <h1 className="hello">Hello World, I'm Mackenzie Kroon</h1>
-          <p>I am a Software Engineer</p>
-          <p className="bottom-border">
-            and a recent graduate from the Grace Hopper program at Fullstack
-            Academy
-          </p>
+          <div className="line-border">
+            <p>I am a Software Engineer</p>
+            <p className="bottom-border">
+              and a recent graduate from the Grace Hopper program at Fullstack
+              Academy
+            </p>
+          </div>
         </div>
       </div>
       <div className="bio-container">
