c9617891c5c6f08240fee7108ba4f69762837e05
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-03-08 15:51:12 -0800

    initial styling

---
+++ src/Components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+
+const Header = (props) => {
+  return (
+    <div className="header">
+      <div>
+        <div className="links-container">
+          <a
+            href="https://www.linkedin.com/in/mackenziekroon/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#F1F4F9" }}
+          >
+            <LinkedInIcon fontSize="large" />
+          </a>
+          <a
+            href="https://github.com/mackenziekroon"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#F1F4F9" }}
+          >
+            <GitHubIcon fontSize="large" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,38 +1,16 @@
 import React from "react";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import GitHubIcon from "@material-ui/icons/GitHub";
 
 const Main = () => {
   return (
-    <div>
-      <div className="heading-container">
-        <div className="links-container">
-          <a
-            href="https://www.linkedin.com/in/mackenziekroon/"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ color: "#2b764c" }}
-          >
-            <LinkedInIcon fontSize="large" />
-          </a>
-          <a
-            href="https://github.com/mackenziekroon"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ color: "#2b764c" }}
-          >
-            <GitHubIcon fontSize="large" />
-          </a>
-        </div>
-        <div className="heading">
-          <h1 className="hello">Hello World, I'm Mackenzie Kroon</h1>
-          <div className="line-border">
-            <p>I am a Software Engineer</p>
-            <p className="bottom-border">
-              and a recent graduate from the Grace Hopper program at Fullstack
-              Academy
-            </p>
-          </div>
+    <div className="heading-container">
+      <div className="heading">
+        <div className="hello">Mackenzie Kroon</div>
+        <div>
+          <div className="software-engineer">Software Engineer</div>
+          <p>
+            and a recent graduate from the Grace Hopper program at Fullstack
+            Academy
+          </p>
         </div>
       </div>
     </div>
