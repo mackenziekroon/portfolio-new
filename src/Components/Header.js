d5c051bf9325c4a574b28e36ceefdebc085a66ef
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-03-08 16:28:52 -0800

    navbar updates

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,30 +5,28 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 const Header = (props) => {
   return (
     <div className="header">
-      <div>
-        <div className="nav-bar">
-          <div>Home</div>
-          <div>Projects</div>
-          <div>Contact Me</div>
-        </div>
-        <div className="links-container">
-          <a
-            href="https://www.linkedin.com/in/mackenziekroon/"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ color: "#F1F4F9" }}
-          >
-            <LinkedInIcon fontSize="large" />
-          </a>
-          <a
-            href="https://github.com/mackenziekroon"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ color: "#F1F4F9" }}
-          >
-            <GitHubIcon fontSize="large" />
-          </a>
-        </div>
+      <div className="nav-bar">
+        <div>Home</div>
+        <div>Projects</div>
+        <div>Contact Me</div>
+      </div>
+      <div className="links-container">
+        <a
+          href="https://www.linkedin.com/in/mackenziekroon/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "#F1F4F9" }}
+        >
+          <LinkedInIcon fontSize="large" />
+        </a>
+        <a
+          href="https://github.com/mackenziekroon"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "#F1F4F9" }}
+        >
+          <GitHubIcon fontSize="large" />
+        </a>
       </div>
     </div>
   );
