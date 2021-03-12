782028e4ddfbefa35807696e4e3abdf440a5edd0
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-03-11 20:41:26 -0800

    sticky navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,38 +2,63 @@ import React from "react";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 
-const Header = (props) => {
-  return (
-    <div className="header">
-      <div className="nav-bar">
-        <div className="headings">Home</div>
-        <a href="#projects" className="headings">
-          Projects
-        </a>
-        <a href="#contact" className="headings">
-          Contact Me
-        </a>
+class Header extends React.Component {
+  listener = null;
+  state = {
+    nav: false,
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll");
+  }
+  handleScroll = () => {
+    if (window.pageYOffset > 140) {
+      if (!this.state.nav) {
+        this.setState({ nav: true });
+      }
+    } else {
+      if (this.state.nav) {
+        this.setState({ nav: false });
+      }
+    }
+  };
+
+  render() {
+    return (
+      <div className="header">
+        <div className="nav-bar">
+          <div className="headings">Home</div>
+          <a href="#projects" className="headings">
+            Projects
+          </a>
+          <a href="#contact" className="headings">
+            Contact Me
+          </a>
+        </div>
+        <div className="links-container">
+          <a
+            href="https://www.linkedin.com/in/mackenziekroon/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#F1F4F9" }}
+          >
+            <LinkedInIcon fontSize="large" />
+          </a>
+          <a
+            href="https://github.com/mackenziekroon"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#F1F4F9" }}
+          >
+            <GitHubIcon fontSize="large" />
+          </a>
+        </div>
       </div>
-      <div className="links-container">
-        <a
-          href="https://www.linkedin.com/in/mackenziekroon/"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: "#F1F4F9" }}
-        >
-          <LinkedInIcon fontSize="large" />
-        </a>
-        <a
-          href="https://github.com/mackenziekroon"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: "#F1F4F9" }}
-        >
-          <GitHubIcon fontSize="large" />
-        </a>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Header;
